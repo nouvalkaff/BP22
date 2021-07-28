07b03c887bd58856fb5507216b20050bdceeff39
M Nouval A Alkaff <82893115+nouvalkaff@users.noreply.github.com>
2021-07-28 19:50:02 +0700

    Add files via upload
    upload test file

---
+++ cobain_1.js
@@ -0,0 +1,6 @@
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("Nama: M Nouval A");
+console.log(`Umur: '20'an, tepatnya "24"`);
+console.log("Hobi: Futsal");
+console.log("Terima kasih");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
