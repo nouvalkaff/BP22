75492dd754cd1f6d1e5c784d5005174231812e8d
M Nouval A Alkaff <82893115+nouvalkaff@users.noreply.github.com>
2021-07-28 19:52:29 +0700

    Delete cobain_1.js

--- cobain_1.js
+++
@@ -1,6 +0,0 @@
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-console.log("Nama: M Nouval A");
-console.log(`Umur: '20'an, tepatnya "24"`);
-console.log("Hobi: Futsal");
-console.log("Terima kasih");
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
